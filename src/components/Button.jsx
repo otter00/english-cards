e59042f1f3dd6a79f7ae0942870bc0d395249a27
otter00 <ivlevanastia2001@mail.ru>
2023-04-04 15:02:31 +0300

    button edit functionality

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 function Button(props) {
-    return <button isClicked={true} className={props.className}>{props.name}</button>;
-}
+    console.log(props)
+      return <button onClick={props.function} className={props.className}>{props.name}</button>;
+  }
 
 export default Button;
--- src/components/TemplateTable.jsx
+++ src/components/TemplateTable.jsx
@@ -1,32 +1,51 @@
-import React from 'react';
-import { useState } from 'react';
+import React,  { useState } from 'react';
 import Button from './Button';
 import ButtonStyle from './styles/Button.module.scss'
 import TableAppearance from './styles/Table.module.scss'
 import TableButton from './styles/TableButton.module.scss'
 import cn from 'classnames';
 
-let buttonEdit = cn([`${TableButton.buttonEdit}`, `${TableButton.generalButton}`]);
-let buttonSave = cn([`${TableButton.buttonSave}`, `${TableButton.generalButton}`]);
-let buttonDelete = cn([`${TableButton.buttonDelete}`, `${TableButton.generalButton}`]);
+let buttonEdit = cn([`${TableButton.buttonEdit}`, ` ${TableButton.generalButton}`]);
+let buttonSave = cn([`${TableButton.buttonSave}`, ` ${TableButton.generalButton}`]);
+let buttonDelete = cn([`${TableButton.buttonDelete}`, ` ${TableButton.generalButton}`]);
 
 export default function Template(props) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [englishWord, setEnglishWord] = useState(props.english);
 
-        const [newWord, setNewWord] = useState(['']);
-
+    const handleEdit = () => {
+        setIsEditing(!isEditing);
+    };
+    
+    const handleCancelEdit = () => {
+        setIsEditing(!isEditing);
+        setEnglishWord(props.english)
+    };
         return (
         <tr>
             <td><span className={TableAppearance.center__flex}>{props.level}</span></td>
-            <td><span className={TableAppearance.center__flex}>{props.english}</span></td>
+            <td>
+              {isEditing ? (
+                    <input
+                      type="text"
+                      value={englishWord}
+                    />
+                  ) : (
+                    <span className={TableAppearance.center__flex}>{props.english}</span>
+                  )}
+            </td>
             <td><span className={TableAppearance.center__flex}>{props.russian}</span></td>
             <td>
                 <div className={TableAppearance.center__flex}>
-                    <Button className={buttonEdit} name={'Edit'} 
-                    // onClick={() => {
-                    //     setNewWord([...newWord, ''])}} 
-                    />
-                    <Button className={buttonSave} name={'Save'}/>
-                    <Button className={buttonDelete} name={'Delete'}/>
+                {isEditing ? (
+                    <>
+                      <Button className={buttonSave} name={'Save'}/>
+                      <Button className={buttonDelete} name={'Delete'} />
+                      <Button className={buttonEdit} name={'Cancel'} function={handleCancelEdit}/>
+                    </>
+                  ) : (
+                    <Button className={buttonEdit} name={'Edit'} function={handleEdit} />
+                  )}
                 </div>
                 {/* {
                     newWord.map((item)=>{
