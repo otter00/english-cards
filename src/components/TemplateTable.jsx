031af9c98d087192bbf6490b102a0053816b7f22
otter00 <ivlevanastia2001@mail.ru>
2023-07-30 12:56:33 +0700

    delete word added

--- src/components/TemplateTable.jsx
+++ src/components/TemplateTable.jsx
@@ -1,225 +1,267 @@
-import React,  { useState, useEffect } from 'react';
-import Button from './Button';
+import React, { useState, useEffect } from "react";
+import Button from "./Button";
 import { observer, inject } from "mobx-react";
-import TableAppearance from './styles/Table.module.scss'
-import TableButton from './styles/TableButton.module.scss'
-import cn from 'classnames';
+import TableAppearance from "./styles/Table.module.scss";
+import TableButton from "./styles/TableButton.module.scss";
+import cn from "classnames";
 
-let buttonEdit = cn([`${TableButton.buttonEdit}`, ` ${TableButton.generalButton}`]);
-let buttonSave = cn([`${TableButton.buttonSave}`, ` ${TableButton.generalButton}`]);
-let buttonCansel = cn([`${TableButton.buttonCansel}`, ` ${TableButton.generalButton}`]);
+let buttonEdit = cn([
+  `${TableButton.buttonEdit}`,
+  ` ${TableButton.generalButton}`,
+]);
+let buttonSave = cn([
+  `${TableButton.buttonSave}`,
+  ` ${TableButton.generalButton}`,
+]);
+let buttonCansel = cn([
+  `${TableButton.buttonCansel}`,
+  ` ${TableButton.generalButton}`,
+]);
 let buttonDisabled = cn([`${TableButton.generalButton__disabled}`]);
-let buttonDelete = cn([`${TableButton.buttonDelete}`, ` ${TableButton.generalButton}`]);
+let buttonDelete = cn([
+  `${TableButton.buttonDelete}`,
+  ` ${TableButton.generalButton}`,
+]);
 
 function Template(props) {
-    const { english, russian, tags, transcription, id, editWord, deleteWord } = props;
-    const [isEditing, setIsEditing] = useState(false);
-    const [word, setWord] = useState({english, russian, tags, transcription, id}); // пропсы из TableWords
-    const [isEmpty, setIsEmpty] = useState(null);
+  const { english, russian, tags, transcription, id, editWord, deleteWord } =
+    props;
+  const [isEditing, setIsEditing] = useState(false);
+  const [word, setWord] = useState({
+    english,
+    russian,
+    tags,
+    transcription,
+    id,
+  }); // пропсы из TableWords
+  const [isEmpty, setIsEmpty] = useState(null);
 
-    // useEffect с зависимостями
-    // при изменении любого из этих свойств в props эффект будет вызван
-    useEffect(() => {
-      setWord({
-        english: props.english,
-        transcription: props.transcription,
-        russian: props.russian,
-        tags: props.tags,
-      });
-    }, [props.english, props.transcription, props.russian, props.tags]);
+  // useEffect с зависимостями
+  // при изменении любого из этих свойств в props эффект будет вызван
+  useEffect(() => {
+    setWord({
+      english: props.english,
+      transcription: props.transcription,
+      russian: props.russian,
+      tags: props.tags,
+    });
+  }, [props.english, props.transcription, props.russian, props.tags]);
 
-    const checkInput = () => {
-      if (word.english.trim() === '' || 
-      word.russian.trim() === '' || 
-      word.tags.trim() === '' || 
-      word.transcription.trim() === '') {
-        setIsEmpty(true);
-      } else {
-        setIsEmpty(false);
-      }
-    };
-
-    useEffect(() => {
-      checkInput();
-    }, [english, russian, tags, transcription]);
-
-    const handleEdit = () => {
-        setIsEditing(!isEditing);
-    };
-    
-    const handleCancelEdit = () => {
-        setWord({ english, russian, tags, transcription });
-        setIsEditing(!isEditing);
-    };
-
-    // function that saves entered word and checks whether field is empty
-    const save = () => {
-        if (word.english.trim() === '' || 
-        word.russian.trim() === '' || 
-        word.tags.trim() === '' ||
-        word.transcription.trim() === '') {
-          setIsEmpty(true);
-          alert(`Please fill all the inputs required`);
-        return;
-        } else if (!word.russian.match("[а-яА-ЯЁё]")) {
-          alert('Please enter a russian word');
-          return;
-        } else if ((!word.english.match("^[a-zA-Z0-9]+$")) ||(!word.tags.match("^[a-zA-Z0-9]+$")) ) {
-          alert('Please enter an english word');
-          return;
-        }
-
-      setIsEditing(!isEditing);
-      // call for function from context to edit word and send it to api
-      // here the editFunc calls when we save changes 
-      // then we send it id and object 'word'
-      // with changes
-      editWord(id, word);
-      console.log(`Form contains english: ${word.english}, transcription: ${word.transcription}, russian: ${word.russian}, tags: ${word.tags}`)
-    }
-
-
-    // onChange funcs contain target value and set it into appropriate field
-
-    const onChangeLevel = (event) => {
-      const value = event.target.value;
-
-      setWord(prevState => ({
-        ...prevState,
-        tags: value,
-      }));
+  const checkInput = () => {
+    if (
+      word.english.trim() === "" ||
+      word.russian.trim() === "" ||
+      word.tags.trim() === "" ||
+      word.transcription.trim() === ""
+    ) {
+      setIsEmpty(true);
+    } else {
       setIsEmpty(false);
     }
+  };
 
-    const onChangeEnglish = (event) => {
-      const value = event.target.value;
+  useEffect(() => {
+    checkInput();
+  }, [english, russian, tags, transcription]);
 
-      setWord(prevState => ({
-        ...prevState,
-        english: value,
-      }));
-      setIsEmpty(false);
+  const handleEdit = () => {
+    setIsEditing(!isEditing);
+  };
+
+  const handleCancelEdit = () => {
+    setWord({ english, russian, tags, transcription });
+    setIsEditing(!isEditing);
+  };
+
+  // function that saves entered word and checks whether field is empty
+  const save = () => {
+    if (
+      word.english.trim() === "" ||
+      word.russian.trim() === "" ||
+      word.tags.trim() === "" ||
+      word.transcription.trim() === ""
+    ) {
+      setIsEmpty(true);
+      alert(`Please fill all the inputs required`);
+      return;
+    } else if (!word.russian.match("[а-яА-ЯЁё]")) {
+      alert("Please enter a russian word");
+      return;
+    } else if (
+      !word.english.match("^[a-zA-Z0-9]+$") ||
+      !word.tags.match("^[a-zA-Z0-9]+$")
+    ) {
+      alert("Please enter an english word");
+      return;
     }
 
-    const onChangeTranscription = (event) => {
-      const value = event.target.value;
+    setIsEditing(!isEditing);
+    // call for function from context to edit word and send it to api
+    // here the editFunc calls when we save changes
+    // then we send it id and object 'word'
+    // with changes
+    editWord(id, word);
+    console.log(
+      `Form contains english: ${word.english}, transcription: ${word.transcription}, russian: ${word.russian}, tags: ${word.tags}`
+    );
+  };
 
-      setWord(prevState => ({
-        ...prevState,
-        transcription: value,
-      }));
-      setIsEmpty(false);
-    }
-  
-    const onChangeRussian = (event) => {
-      const value = event.target.value;
+  // onChange funcs contain target value and set it into appropriate field
 
-      setWord(prevState => ({
-        ...prevState,
-        russian: value,
-      }));
-      setIsEmpty(false);
-    }
+  const onChangeLevel = (event) => {
+    const value = event.target.value;
+
+    setWord((prevState) => ({
+      ...prevState,
+      tags: value,
+    }));
+    setIsEmpty(false);
+  };
+
+  const onChangeEnglish = (event) => {
+    const value = event.target.value;
+
+    setWord((prevState) => ({
+      ...prevState,
+      english: value,
+    }));
+    setIsEmpty(false);
+  };
+
+  const onChangeTranscription = (event) => {
+    const value = event.target.value;
+
+    setWord((prevState) => ({
+      ...prevState,
+      transcription: value,
+    }));
+    setIsEmpty(false);
+  };
+
+  const onChangeRussian = (event) => {
+    const value = event.target.value;
+
+    setWord((prevState) => ({
+      ...prevState,
+      russian: value,
+    }));
+    setIsEmpty(false);
+  };
 
   // handleDeleteWord вызывает функцию deleteWord с двумя аргументами: id и wordToDelete.
- //id - id удаляемого слова
- // wordToDelete - объект со свойствами english, transcription, russian, tags
-    const handleDeleteWord = () => {
-      const wordToDelete = {
-        english: word.english,
-        transcription: word.transcription,
-        russian: word.russian,
-        tags: word.tags,
-      };
-      deleteWord(id, wordToDelete);
-      window.location.reload();
-    }
+  //id - id удаляемого слова
+  // wordToDelete - объект со свойствами english, transcription, russian, tags
+  const handleDeleteWord = () => {
+    const wordToDelete = {
+      english: word.english,
+      transcription: word.transcription,
+      russian: word.russian,
+      tags: word.tags,
+    };
+    deleteWord(id, wordToDelete);
+  };
 
-        return (
-        <tr>
-            <td>
-              {isEditing? (
-                    <input
-                    onChange={onChangeLevel}
-                    type="text"
-                    value={word.tags}
-                    // check whether field is empty
-                    // if true, set the class wth warning frame
-                    className={word.tags.trim() === '' ? `${TableAppearance.empty_input}` : ''}
-                  />
-              ) : ( 
-                <span className={TableAppearance.center__flex}>{word.tags}</span>
-                )}
-              </td>
-            <td>
-              {isEditing ? (
-                    <input
-                      onChange={onChangeEnglish}
-                      type="text"
-                      value={word.english}
-                      className={word.english.trim() === '' ? `${TableAppearance.empty_input}` : ''}
-                    />
-                  ) : (
-                    <span className={TableAppearance.center__flex}>{word.english}</span>
-                  )}
-            </td>
-            <td>
-            {isEditing ? (
-                    <input
-                    onChange={onChangeTranscription}
-                    type="text"
-                    value={word.transcription}
-                    className={word.transcription.trim() === '' ? `${TableAppearance.empty_input}` : ''}
-                  />
-            ) : (
-                  <span className={TableAppearance.center__flex}>{word.transcription}</span>
-            )}
-            </td>
-            <td>
-            {isEditing ? (
-                    <input
-                    onChange={onChangeRussian}
-                    type="text"
-                    value={word.russian}
-                    className={word.russian.trim() === '' ? `${TableAppearance.empty_input}` : ''}
-                  />
-            ) : (
-                  <span className={TableAppearance.center__flex}>{word.russian}</span>
-            )}
-            </td>
-            <td>
-                <div className={TableAppearance.center__flex}>
-                {isEditing ? (
-                    <>
-                      <Button                   
-                      onClick={save} 
-                      className={isEmpty ? `${buttonDisabled}` : `${buttonSave}`} 
-                      name={'Save'}
-                      disabled={isEmpty}/>
+  return (
+    <tr>
+      <td>
+        {isEditing ? (
+          <input
+            onChange={onChangeLevel}
+            type="text"
+            value={word.tags}
+            // check whether field is empty
+            // if true, set the class wth warning frame
+            className={
+              word.tags.trim() === "" ? `${TableAppearance.empty_input}` : ""
+            }
+          />
+        ) : (
+          <span className={TableAppearance.center__flex}>{word.tags}</span>
+        )}
+      </td>
+      <td>
+        {isEditing ? (
+          <input
+            onChange={onChangeEnglish}
+            type="text"
+            value={word.english}
+            className={
+              word.english.trim() === "" ? `${TableAppearance.empty_input}` : ""
+            }
+          />
+        ) : (
+          <span className={TableAppearance.center__flex}>{word.english}</span>
+        )}
+      </td>
+      <td>
+        {isEditing ? (
+          <input
+            onChange={onChangeTranscription}
+            type="text"
+            value={word.transcription}
+            className={
+              word.transcription.trim() === ""
+                ? `${TableAppearance.empty_input}`
+                : ""
+            }
+          />
+        ) : (
+          <span className={TableAppearance.center__flex}>
+            {word.transcription}
+          </span>
+        )}
+      </td>
+      <td>
+        {isEditing ? (
+          <input
+            onChange={onChangeRussian}
+            type="text"
+            value={word.russian}
+            className={
+              word.russian.trim() === "" ? `${TableAppearance.empty_input}` : ""
+            }
+          />
+        ) : (
+          <span className={TableAppearance.center__flex}>{word.russian}</span>
+        )}
+      </td>
+      <td>
+        <div className={TableAppearance.center__flex}>
+          {isEditing ? (
+            <>
+              <Button
+                onClick={save}
+                className={isEmpty ? `${buttonDisabled}` : `${buttonSave}`}
+                name={"Save"}
+                disabled={isEmpty}
+              />
 
-                      <Button 
-                      className={buttonCansel} 
-                      name={'Cancel'} 
-                      onClick={handleCancelEdit}/>
-                    </>
-                  ) : (
-                    <>
-                    <Button 
-                    className={buttonEdit} 
-                    name={'Edit'} 
-                    onClick={handleEdit} />
+              <Button
+                className={buttonCansel}
+                name={"Cancel"}
+                onClick={handleCancelEdit}
+              />
+            </>
+          ) : (
+            <>
+              <Button
+                className={buttonEdit}
+                name={"Edit"}
+                onClick={handleEdit}
+              />
 
-                    <Button 
-                    className={buttonDelete}
-                    name={'Delete'}
-                    // handleDeleteWord привязана к событию onClick кнопки
-                    onClick={handleDeleteWord} />
-                    </>
-                  )}
-                </div>
-            </td>
-        </tr>
-    );
+              <Button
+                className={buttonDelete}
+                name={"Delete"}
+                // handleDeleteWord привязана к событию onClick кнопки
+                onClick={handleDeleteWord}
+              />
+            </>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
 }
 
 export default inject(({ wordsData }) => ({
--- src/components/store/storeWords.js
+++ src/components/store/storeWords.js
@@ -30,175 +30,190 @@ class Words {
     // то теперь меняя данные в MobX
     // надо явно указать на эти изменения
     // с помощью функции runInAction
-runInAction(() => {
-  this.words = data;
-  this.isLoading = false;
-  this.isLoaded = true;
-});
-};
-
-addWord = async (id, newWord) => {
-  this.isLoading = true;
-  
-  try {
-    const response = await fetch(
-      `http://itgirlschool.justmakeit.ru/api/words/${id}/add`,
-      {
-        method: "POST",
-        mode: "cors",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newWord),
-      }
-    );
-
-    if (response.ok) {
-      runInAction(() => {
-        this.words.push(newWord);
-      });
-    } else {
-      throw new Error("Try again");
-    }
-  } catch (error) {
     runInAction(() => {
-      this.error = error;
-    });
-    return "Failed to add word";
-  } finally {
-    runInAction(() => {
+      this.words = data;
       this.isLoading = false;
+      this.isLoaded = true;
     });
-  }
-};
+  };
 
-//   addWord = async (id, newWord) => {
-//     //this.isLoading = true;
-//     console.log("addWord");
-//     // try {
-//     //   fetch(`http://itgirlschool.justmakeit.ru/api/words/${id}/add`, {
-//     //     method: "POST",
-//     //     mode: "cors",
-//     //     headers: {
-//     //       "Content-Type": "application/json",
-//     //     },
-//     //     body: JSON.stringify(newWord),
-//     //   });
+  addWord = async (id, newWord) => {
+    this.isLoading = true;
 
-    
-//   try {
-//     const response = await fetch(
-//       `http://itgirlschool.justmakeit.ru/api/words/${id}/add`,
-//       {
-//         method: "POST",
-//         mode: "cors",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(newWord),
-//       }
-//     );
+    try {
+      const response = await fetch(
+        `http://itgirlschool.justmakeit.ru/api/words/${id}/add`,
+        {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newWord),
+        }
+      );
 
-//       if (response.ok) {
-//         runInAction(() => {
-//           this.words = this.words.map((word) =>
-//             word.id === id ? { ...word, ...newWord } : word
-//           );
-//         });
-//       } else {
-//         throw new Error("Try again");
-//       }
-//     } catch (error) {
-//       runInAction(() => {
-//         this.error = error;
-//       });
-//       return "Failed to add word";
-//     } finally {
-//       runInAction(() => {
-//         this.words = newWord;
-//         this.words.push(newWord);
-//         this.isLoading = false;
-//       });
-//     }
-
-// //         .then((response) => {
-// //           if (response.ok) {
-// //             return response.json();
-// //           } else {
-// //             throw new Error("Try again");
-// //           }
-// //         })
-// //         .then(() => {
-// //           this.isLoading = false;
-// //           this.words.push(newWord);
-// //         });
-// //     } catch (error) {
-// //       this.isLoading = false;
-// //       this.error = error;
-// //       return "Fail to add word";
-// //     }
-// //     runInAction(() => {
-// //   this.isLoading = false;
-// //   this.isLoaded = true;
-// // });
-//   }
-
-editWord = async (id, newWord) => {
-  this.isLoading = true;
-
-  try {
-    const response = await fetch(
-      `http://itgirlschool.justmakeit.ru/api/words/${id}/update`,
-      {
-        method: "POST",
-        mode: "cors",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newWord),
+      if (response.ok) {
+        runInAction(() => {
+          this.words.push(newWord);
+        });
+      } else {
+        throw new Error("Try again");
       }
-    );
-
-    if (response.ok) {
+    } catch (error) {
       runInAction(() => {
-        this.words = this.words.map((word) =>
-          word.id === id ? { ...word, ...newWord } : word
-        );
+        this.error = error;
       });
-    } else {
-      throw new Error("Try again");
+      return "Failed to add word";
+    } finally {
+      runInAction(() => {
+        this.isLoading = false;
+      });
     }
-  } catch (error) {
-    runInAction(() => {
-      this.error = error;
-    });
-    return "Failed to edit word";
-  } finally {
-    runInAction(() => {
-      this.isLoading = false;
-    });
-  }
-};
+  };
 
-  deleteWord = async (id, word) => {
+  //   addWord = async (id, newWord) => {
+  //     //this.isLoading = true;
+  //     console.log("addWord");
+  //     // try {
+  //     //   fetch(`http://itgirlschool.justmakeit.ru/api/words/${id}/add`, {
+  //     //     method: "POST",
+  //     //     mode: "cors",
+  //     //     headers: {
+  //     //       "Content-Type": "application/json",
+  //     //     },
+  //     //     body: JSON.stringify(newWord),
+  //     //   });
+
+  //   try {
+  //     const response = await fetch(
+  //       `http://itgirlschool.justmakeit.ru/api/words/${id}/add`,
+  //       {
+  //         method: "POST",
+  //         mode: "cors",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //         body: JSON.stringify(newWord),
+  //       }
+  //     );
+
+  //       if (response.ok) {
+  //         runInAction(() => {
+  //           this.words = this.words.map((word) =>
+  //             word.id === id ? { ...word, ...newWord } : word
+  //           );
+  //         });
+  //       } else {
+  //         throw new Error("Try again");
+  //       }
+  //     } catch (error) {
+  //       runInAction(() => {
+  //         this.error = error;
+  //       });
+  //       return "Failed to add word";
+  //     } finally {
+  //       runInAction(() => {
+  //         this.words = newWord;
+  //         this.words.push(newWord);
+  //         this.isLoading = false;
+  //       });
+  //     }
+
+  // //         .then((response) => {
+  // //           if (response.ok) {
+  // //             return response.json();
+  // //           } else {
+  // //             throw new Error("Try again");
+  // //           }
+  // //         })
+  // //         .then(() => {
+  // //           this.isLoading = false;
+  // //           this.words.push(newWord);
+  // //         });
+  // //     } catch (error) {
+  // //       this.isLoading = false;
+  // //       this.error = error;
+  // //       return "Fail to add word";
+  // //     }
+  // //     runInAction(() => {
+  // //   this.isLoading = false;
+  // //   this.isLoaded = true;
+  // // });
+  //   }
+
+  editWord = async (id, newWord) => {
+    this.isLoading = true;
+
+    try {
+      const response = await fetch(
+        `http://itgirlschool.justmakeit.ru/api/words/${id}/update`,
+        {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newWord),
+        }
+      );
+
+      if (response.ok) {
+        runInAction(() => {
+          this.words = this.words.map((word) =>
+            word.id === id ? { ...word, ...newWord } : word
+          );
+        });
+      } else {
+        throw new Error("Try again");
+      }
+    } catch (error) {
+      runInAction(() => {
+        this.error = error;
+      });
+      return "Failed to edit word";
+    } finally {
+      runInAction(() => {
+        this.isLoading = false;
+      });
+    }
+  };
+
+  deleteWord = async (id, newWord) => {
     console.log("delete");
     try {
-      const data = await fetch(`/api/words/${id}/delete`, {
-        method: "POST",
-        mode: "cors",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(word),
+      const data = await fetch(
+        `http://itgirlschool.justmakeit.ru/api/words/${id}/delete`,
+        {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newWord),
+        }
+      );
+
+      if (data.ok) {
+        runInAction(() => {
+          this.words = this.words.map((word) =>
+            word.filter((word) => word.id !== id)
+          );
+        });
+      } else {
+        throw new Error("Try again");
+      }
+    } catch (error) {
+      runInAction(() => {
+        this.error = error;
       });
-
-      const json = await data.json();
-
-      console.log(json);
-    } catch (err) {
-      return "Fail to delete word";
+      return "Failed to delte word";
+    } finally {
+      runInAction(() => {
+        this.isLoading = false;
+      });
     }
-  }
+  };
 }
 
 export default Words;
