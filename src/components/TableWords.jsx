2960c5efdfc0370d8adf9f7019c0a8e206e0f6d9
otter00 <ivlevanastia2001@mail.ru>
2023-08-23 16:56:06 +0700

    structure changes

--- src/components/TableWords.jsx
+++ src/components/TableWords.jsx
@@ -1,55 +1,52 @@
-import CardWordsJson from './CardWords';
-import SchoolWords from './SchoolJSON';
-import Template from './TemplateTable';
-import React from 'react';
-import styled from 'styled-components';
+import Template from "./TemplateTable";
+import React from "react";
+import styled from "styled-components";
 
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { WordsContext } from "../context/ContextProvider";
 
 const Loading = styled.div`
-color: white;
-display: flex;
-justify-content: center;
-text-transform: uppercase;
-font-size: 54px;
-font-weight: 600;
-`
+  color: white;
+  display: flex;
+  justify-content: center;
+  text-transform: uppercase;
+  font-size: 54px;
+  font-weight: 600;
+`;
 
 export default function TableWords() {
-    const context = useContext(WordsContext);
-    const words = context.words;
+  const context = useContext(WordsContext);
+  const words = context.words;
 
-    // JSON with words from school
+  // JSON with words from school
 
-    //let CardWords = JSON.parse(SchoolWords);
-    //console.log(CardWords);
+  //let CardWords = JSON.parse(SchoolWords);
+  //console.log(CardWords);
 
-    // here we use words got from context
-    if (!words) {
-        return <Loading>Loading...</Loading>;
-      } else if (words) {
-        console.log(words);
-      }
-    
-    return (
-        <>
-        <tbody> {
-            words.map((word, id) =>
-                <Template
-                // key & id we get from JSON as id & tags_json
-                key={id} // id
-                id={word.id} // tags_json
-                tags={word.tags}
-                english={word.english}
-                transcription={word.transcription}
-                russian={word.russian}
+  // here we use words got from context
+  if (!words) {
+    return <Loading>Loading...</Loading>;
+  } else if (words) {
+    console.log(words);
+  }
 
-                name={'Edit'}
-                ></Template>
-            )
-        }
-        </tbody>
-        </>
-    )
+  return (
+    <>
+      <tbody>
+        {" "}
+        {words.map((word, id) => (
+          <Template
+            // key & id we get from JSON as id & tags_json
+            key={id} // id
+            id={word.id} // tags_json
+            tags={word.tags}
+            english={word.english}
+            transcription={word.transcription}
+            russian={word.russian}
+            name={"Edit"}
+          ></Template>
+        ))}
+      </tbody>
+    </>
+  );
 }
--- src/components/TemplateTable.jsx
+++ src/components/TemplateTable.jsx
@@ -1,238 +1,278 @@
-import React,  { useState, useContext, useEffect } from 'react';
-import Button from './Button';
-import { WordsContext } from '../context/ContextProvider';
-import TableAppearance from './styles/Table.module.scss';
-import TableButton from './styles/TableButton.module.scss';
-import cn from 'classnames';
+import React, { useState, useContext, useEffect } from "react";
+import Button from "./Button";
+import { WordsContext } from "../context/ContextProvider";
+import TableAppearance from "./styles/Table.module.scss";
+import TableButton from "./styles/TableButton.module.scss";
+import cn from "classnames";
 
-let buttonEdit = cn([`${TableButton.buttonEdit}`, ` ${TableButton.generalButton}`]);
-let buttonSave = cn([`${TableButton.buttonSave}`, ` ${TableButton.generalButton}`]);
-let buttonCansel = cn([`${TableButton.buttonCansel}`, ` ${TableButton.generalButton}`]);
+let buttonEdit = cn([
+  `${TableButton.buttonEdit}`,
+  ` ${TableButton.generalButton}`,
+]);
+let buttonSave = cn([
+  `${TableButton.buttonSave}`,
+  ` ${TableButton.generalButton}`,
+]);
+let buttonCansel = cn([
+  `${TableButton.buttonCansel}`,
+  ` ${TableButton.generalButton}`,
+]);
 let buttonDisabled = cn([`${TableButton.generalButton__disabled}`]);
-let buttonDelete = cn([`${TableButton.buttonDelete}`, ` ${TableButton.generalButton}`]);
+let buttonDelete = cn([
+  `${TableButton.buttonDelete}`,
+  ` ${TableButton.generalButton}`,
+]);
 
 export default function Template(props) {
-    let { english, russian, tags, transcription, id } = props;
-    const [isEditing, setIsEditing] = useState(false);
-    const [word, setWord] = useState({english, russian, tags, transcription}); // пропсы из TableWords
-    const [isEmpty, setIsEmpty] = useState(null);
-    const { deleteWord, editWord } = useContext(WordsContext); // call deleteWord from the context 
-    // and set in into variable deleteWord
+  let { english, russian, tags, transcription, id } = props;
+  const [isEditing, setIsEditing] = useState(false);
+  const [word, setWord] = useState({ english, russian, tags, transcription }); // пропсы из TableWords
+  const [isEmpty, setIsEmpty] = useState(null);
+  const { deleteWord, editWord } = useContext(WordsContext); // call deleteWord from the context
+  // and set in into variable deleteWord
 
-    // useEffect с зависимостями
-    // при изменении любого из этих свойств в props эффект будет вызван
-    useEffect(() => {
-      setWord({
-        english: props.english,
-        transcription: props.transcription,
-        russian: props.russian,
-        tags: props.tags,
-      });
-    }, [props.english, props.transcription, props.russian, props.tags]);
+  // useEffect с зависимостями
+  // при изменении любого из этих свойств в props эффект будет вызван
+  useEffect(() => {
+    setWord({
+      english: props.english,
+      transcription: props.transcription,
+      russian: props.russian,
+      tags: props.tags,
+    });
+  }, [props.english, props.transcription, props.russian, props.tags]);
 
-    const checkInput = () => {
-      if (word.english.trim() === '' || 
-      word.russian.trim() === '' || 
-      word.tags.trim() === '' || 
-      word.transcription.trim() === '') {
-        setIsEmpty(true);
-      } else {
-        setIsEmpty(false);
-      }
+  const checkInput = () => {
+    if (
+      word.english === "" ||
+      word.russian === "" ||
+      word.tags === "" ||
+      word.transcription === ""
+    ) {
+      setIsEmpty(true);
+    } else {
+      setIsEmpty(false);
+    }
+  };
+
+  useEffect(() => {
+    console.log("mount");
+    return () => {
+      console.log("unmount");
     };
+  }, []);
 
-    useEffect(() => {
-      console.log('mount');
-      return() => {
-        console.log('unmount');
-      }
-    }, [])
+  useEffect(() => {
+    checkInput();
+  }, [english, russian, tags, transcription]);
 
-    useEffect(() => {
-      checkInput();
-    }, [english, russian, tags, transcription]);
+  const handleEdit = () => {
+    setIsEditing(!isEditing);
+  };
 
-    const handleEdit = () => {
-        setIsEditing(!isEditing);
-    };
-    
-    const handleCancelEdit = () => {
-        setWord({ english, russian, tags, transcription });
-        setIsEditing(!isEditing);
-    };
+  const handleCancelEdit = () => {
+    setWord({ english, russian, tags, transcription });
+    setIsEditing(!isEditing);
+  };
 
-    // function delString() {
-    //   setWord({});
-    // }
+  // function delString() {
+  //   setWord({});
+  // }
 
-    // function that saves entered word and checks whether field is empty
-    const save = () => {
-        if (word.english.trim() === '' || 
-        word.russian.trim() === '' || 
-        word.tags.trim() === '' ||
-        word.transcription.trim() === '') {
-          setIsEmpty(true);
-          alert(`Please fill all the inputs required`);
-        return;
-        } else if (!word.russian.match("[а-яА-ЯЁё]")) {
-          alert('Please enter a russian word');
-          return;
-        } else if ((!word.english.match("^[a-zA-Z0-9]+$")) ||(!word.tags.match("^[a-zA-Z0-9]+$")) ) {
-          alert('Please enter an english word');
-          return;
-        }
-
-      setIsEditing(!isEditing);
-      // call for function from context to edit word and send it to api
-      editWord(id, word);
-      window.location.reload();
-      // here the editFunc calls when we save changes 
-      // then we send it id and object 'word'
-      // with changes
-      console.log(`Form contains english: ${word.english}, transcription: ${word.transcription}, russian: ${word.russian}, tags: ${word.tags}`)
+  // function that saves entered word and checks whether field is empty
+  const save = () => {
+    if (
+      word.english.trim() === "" ||
+      word.russian.trim() === "" ||
+      word.tags.trim() === "" ||
+      word.transcription.trim() === ""
+    ) {
+      setIsEmpty(true);
+      alert(`Please fill all the inputs required`);
+      return;
+    } else if (!word.russian.match("[а-яА-ЯЁё]")) {
+      alert("Please enter a russian word");
+      return;
+    } else if (
+      !word.english.match("^[a-zA-Z0-9]+$") ||
+      !word.tags.match("^[a-zA-Z0-9]+$")
+    ) {
+      alert("Please enter an english word");
+      return;
     }
 
+    setIsEditing(!isEditing);
+    // call for function from context to edit word and send it to api
+    editWord(id, word);
+    window.location.reload();
+    // here the editFunc calls when we save changes
+    // then we send it id and object 'word'
+    // with changes
+    console.log(
+      `Form contains english: ${word.english}, transcription: ${word.transcription}, russian: ${word.russian}, tags: ${word.tags}`
+    );
+  };
 
-    // onChange funcs contain target value and set it into appropriate field
+  // onChange funcs contain target value and set it into appropriate field
 
-    const onChangeLevel = (event) => {
-      const value = event.target.value;
+  const onChangeLevel = (event) => {
+    const value = event.target.value;
 
-      setWord(prevState => ({
-        ...prevState,
-        tags: value,
-      }));
-      setIsEmpty(false);
-    }
+    setWord((prevState) => ({
+      ...prevState,
+      tags: value,
+    }));
+    setIsEmpty(false);
+  };
 
-    const onChangeEnglish = (event) => {
-      const value = event.target.value;
+  const onChangeEnglish = (event) => {
+    const value = event.target.value;
 
-      setWord(prevState => ({
-        ...prevState,
-        english: value,
-      }));
-      setIsEmpty(false);
-    }
+    setWord((prevState) => ({
+      ...prevState,
+      english: value,
+    }));
+    setIsEmpty(false);
+  };
 
-    const onChangeTranscription = (event) => {
-      const value = event.target.value;
+  const onChangeTranscription = (event) => {
+    const value = event.target.value;
 
-      setWord(prevState => ({
-        ...prevState,
-        transcription: value,
-      }));
-      setIsEmpty(false);
-    }
-  
-    const onChangeRussian = (event) => {
-      const value = event.target.value;
+    setWord((prevState) => ({
+      ...prevState,
+      transcription: value,
+    }));
+    setIsEmpty(false);
+  };
 
-      setWord(prevState => ({
-        ...prevState,
-        russian: value,
-      }));
-      setIsEmpty(false);
-    }
+  const onChangeRussian = (event) => {
+    const value = event.target.value;
+
+    setWord((prevState) => ({
+      ...prevState,
+      russian: value,
+    }));
+    setIsEmpty(false);
+  };
 
   // handleDeleteWord вызывает функцию deleteWord с двумя аргументами: id и wordToDelete.
- //id - id удаляемого слова
- // wordToDelete - объект со свойствами english, transcription, russian, tags
-    const handleDeleteWord = () => {
-      const wordToDelete = {
-        english: word.english,
-        transcription: word.transcription,
-        russian: word.russian,
-        tags: word.tags,
-      };
-      deleteWord(id, wordToDelete);
-    }
+  //id - id удаляемого слова
+  // wordToDelete - объект со свойствами english, transcription, russian, tags
+  const handleDeleteWord = () => {
+    const wordToDelete = {
+      english: word.english,
+      transcription: word.transcription,
+      russian: word.russian,
+      tags: word.tags,
+    };
+    deleteWord(id, wordToDelete);
+  };
 
-        return (
-          <>
-        <tr>
-            <td>
-              {isEditing? (
-                    <input
-                    onChange={onChangeLevel}
-                    type="text"
-                    value={word.tags}
-                    // check whether field is empty
-                    // if true, set the class wth warning frame
-                    className={word.tags.trim() === '' ? `${TableAppearance.empty_input}` : ''}
-                  />
-              ) : ( 
-                <span className={TableAppearance.center__flex}>{word.tags}</span>
-                )}
-              </td>
-            <td>
-              {isEditing ? (
-                    <input
-                      onChange={onChangeEnglish}
-                      type="text"
-                      value={word.english}
-                      className={word.english.trim() === '' ? `${TableAppearance.empty_input}` : ''}
-                    />
-                  ) : (
-                    <span className={TableAppearance.center__flex}>{word.english}</span>
-                  )}
-            </td>
-            <td>
+  return (
+    <>
+      <tr>
+        <td>
+          {isEditing ? (
+            <input
+              onChange={onChangeLevel}
+              type="text"
+              value={word.tags}
+              // check whether field is empty
+              // if true, set the class wth warning frame
+              className={
+                word.tags.trim() === "" ? `${TableAppearance.empty_input}` : ""
+              }
+            />
+          ) : (
+            <span className={TableAppearance.center__flex}>{word.tags}</span>
+          )}
+        </td>
+        <td>
+          {isEditing ? (
+            <input
+              onChange={onChangeEnglish}
+              type="text"
+              value={word.english}
+              className={
+                word.english.trim() === ""
+                  ? `${TableAppearance.empty_input}`
+                  : ""
+              }
+            />
+          ) : (
+            <span className={TableAppearance.center__flex}>{word.english}</span>
+          )}
+        </td>
+        <td>
+          {isEditing ? (
+            <input
+              onChange={onChangeTranscription}
+              type="text"
+              value={word.transcription}
+              className={
+                word.transcription.trim() === ""
+                  ? `${TableAppearance.empty_input}`
+                  : ""
+              }
+            />
+          ) : (
+            <span className={TableAppearance.center__flex}>
+              {word.transcription}
+            </span>
+          )}
+        </td>
+        <td>
+          {isEditing ? (
+            <input
+              onChange={onChangeRussian}
+              type="text"
+              value={word.russian}
+              className={
+                word.russian.trim() === ""
+                  ? `${TableAppearance.empty_input}`
+                  : ""
+              }
+            />
+          ) : (
+            <span className={TableAppearance.center__flex}>{word.russian}</span>
+          )}
+        </td>
+        <td>
+          <div className={TableAppearance.center__flex}>
             {isEditing ? (
-                    <input
-                    onChange={onChangeTranscription}
-                    type="text"
-                    value={word.transcription}
-                    className={word.transcription.trim() === '' ? `${TableAppearance.empty_input}` : ''}
-                  />
-            ) : (
-                  <span className={TableAppearance.center__flex}>{word.transcription}</span>
-            )}
-            </td>
-            <td>
-            {isEditing ? (
-                    <input
-                    onChange={onChangeRussian}
-                    type="text"
-                    value={word.russian}
-                    className={word.russian.trim() === '' ? `${TableAppearance.empty_input}` : ''}
-                  />
-            ) : (
-                  <span className={TableAppearance.center__flex}>{word.russian}</span>
-            )}
-            </td>
-            <td>
-                <div className={TableAppearance.center__flex}>
-                {isEditing ? (
-                    <>
-                      <Button                   
-                      onClick={save} 
-                      className={isEmpty ? `${buttonDisabled}` : `${buttonSave}`} 
-                      name={'Save'}
-                      disabled={isEmpty}/>
+              <>
+                <Button
+                  onClick={save}
+                  className={isEmpty ? `${buttonDisabled}` : `${buttonSave}`}
+                  name={"Save"}
+                  disabled={isEmpty}
+                />
 
-                      <Button 
-                      className={buttonCansel} 
-                      name={'Cancel'} 
-                      onClick={handleCancelEdit}/>
-                    </>
-                  ) : (
-                    <>
-                    <Button 
-                    className={buttonEdit} 
-                    name={'Edit'} 
-                    onClick={handleEdit} />
+                <Button
+                  className={buttonCansel}
+                  name={"Cancel"}
+                  onClick={handleCancelEdit}
+                />
+              </>
+            ) : (
+              <>
+                <Button
+                  className={buttonEdit}
+                  name={"Edit"}
+                  onClick={handleEdit}
+                />
 
-                    <Button 
-                    className={buttonDelete}
-                    name={'Delete'}
-                    // handleDeleteWord привязана к событию onClick кнопки
-                    onClick={handleDeleteWord} />
-                    </>
-                  )}
-                </div>
-            </td>
-        </tr>
-        </>
-    );
+                <Button
+                  className={buttonDelete}
+                  name={"Delete"}
+                  // handleDeleteWord привязана к событию onClick кнопки
+                  onClick={handleDeleteWord}
+                />
+              </>
+            )}
+          </div>
+        </td>
+      </tr>
+    </>
+  );
 }
