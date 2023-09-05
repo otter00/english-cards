92bc3bd622d4d4e11a4a94df3a499f368c7ac85e
otter00 <ivlevanastia2001@mail.ru>
2023-09-05 15:03:39 +0700

    card component

--- src/components/CardSlider.jsx
+++ src/components/CardSlider.jsx
@@ -1,79 +1,78 @@
-import React, {useState, useRef, useCallback} from 'react';
-import './styles/styles.scss';
+import React, { useState, useRef, useCallback } from "react";
+import "./styles/styles.scss";
 // import components
-import Card from './Card';
-import FinalCard from './EndOfCard';
+import Card from "./card";
+import FinalCard from "./EndOfCard";
 import JsonWords from "./CardWords";
 
 let words = JSON.parse(JsonWords);
 
 const CardSlider = () => {
-    const [cardIndex, setCardIndex] = useState(0);
-    const [idxWordsTranslated, setIdxWordsTranslated] = useState([]);
-    
-    const buttonTranslateRef = useRef(null);
+  const [cardIndex, setCardIndex] = useState(0);
+  const [idxWordsTranslated, setIdxWordsTranslated] = useState([]);
 
-    const setButtonTranslateRef = useCallback((node) => {
+  const buttonTranslateRef = useRef(null);
+
+  const setButtonTranslateRef = useCallback((node) => {
     if (node) {
-        buttonTranslateRef.current = node;
-    }}, []);
-
-    function nextCard() {
-        if (buttonTranslateRef.current) {
-            buttonTranslateRef.current.focus();
-        }
-        setCardIndex(cardIndex + 1);
+      buttonTranslateRef.current = node;
     }
+  }, []);
 
-    function previousCard() {
-        setCardIndex(cardIndex - 1);
+  function nextCard() {
+    if (buttonTranslateRef.current) {
+      buttonTranslateRef.current.focus();
     }
+    setCardIndex(cardIndex + 1);
+  }
 
-    function startSlider() {
-        setCardIndex(0);
-        setIdxWordsTranslated([]);
-    }
+  function previousCard() {
+    setCardIndex(cardIndex - 1);
+  }
 
-    function onClickTranslate() {
-        setIdxWordsTranslated((prev) => {
-            if (!idxWordsTranslated.includes(cardIndex)) {
-                return [
-                    ...prev,
-                    cardIndex
-                ]
-            }
-            return prev;
-        })
-    }
+  function startSlider() {
+    setCardIndex(0);
+    setIdxWordsTranslated([]);
+  }
 
-    const countTranslated = idxWordsTranslated.length;
+  function onClickTranslate() {
+    setIdxWordsTranslated((prev) => {
+      if (!idxWordsTranslated.includes(cardIndex)) {
+        return [...prev, cardIndex];
+      }
+      return prev;
+    });
+  }
 
-    if(words) {
-        return(
-            <>
-                {cardIndex < words.length && (
-                    <>
-                        <Card 
-                        previousCard={previousCard} 
-                        onClickTranslate={onClickTranslate} 
-                        word={words[cardIndex]} 
-                        nextCard={nextCard}
-                        ref={setButtonTranslateRef}
-                        />
-                        {cardIndex + 1} / {words.length}
-                    </>
-                    )}
-                {cardIndex === words.length && (
-                    <>
-                        <FinalCard 
-                        startSlider={startSlider} 
-                        countTranslated={countTranslated}/>
-                    </>
-                    )}
-            </>
-        )
-    }
-    alert("Service is unavailable");
-}
+  const countTranslated = idxWordsTranslated.length;
+
+  if (words) {
+    return (
+      <>
+        {cardIndex < words.length && (
+          <>
+            <Card
+              previousCard={previousCard}
+              onClickTranslate={onClickTranslate}
+              word={words[cardIndex]}
+              nextCard={nextCard}
+              ref={setButtonTranslateRef}
+            />
+            {cardIndex + 1} / {words.length}
+          </>
+        )}
+        {cardIndex === words.length && (
+          <>
+            <FinalCard
+              startSlider={startSlider}
+              countTranslated={countTranslated}
+            />
+          </>
+        )}
+      </>
+    );
+  }
+  alert("Service is unavailable");
+};
 
 export default CardSlider;
