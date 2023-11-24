7dd7222e03d82c806d4cef3d7322d7a65c6fabc7
otter00 <ivlevanastia2001@mail.ru>
2023-11-24 21:12:33 +0300

    api words + axios added

--- src/components/TableWords.jsx
+++ src/components/TableWords.jsx
@@ -1,27 +1,57 @@
-import CardWordsJson from './CardWords';
-import SchoolWords from './SchoolJSON';
-import Template from './TemplateTable';
-import React from 'react';
+import CardWordsJson from "./CardWords";
+import SchoolWords from "./SchoolJSON";
+import Template from "./TemplateTable";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function TableWords() {
-    let CardWords = JSON.parse(SchoolWords);
-    console.log(CardWords);
-    return (
-        <tbody> {
-            CardWords.map((word, id) =>
-                <Template
-                key={id}
-                tags={word.tags}
-                english={word.english}
-                transcription={word.transcription}
-                russian={word.russian}
+  const [english, setEnglish] = useState([]);
 
-                name={'Edit'}
-                ></Template>
-            )
-        }
-        </tbody>
-    )
+  useEffect(() => {
+    axios
+      .get(
+        `https://6536ba1dbb226bb85dd28e56.mockapi.io/api/diploma_blog/english_game`
+      )
+      .then((response) => {
+        setEnglish(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <tbody>
+      {english.map((word) => (
+        <Template
+          key={word.id}
+          tags={word.tags}
+          english={word.english}
+          transcription={word.transcription}
+          russian={word.russian}
+          name={"Edit"}
+        ></Template>
+      ))}
+    </tbody>
+  );
+
+  //let CardWords = JSON.parse(SchoolWords);
+  //console.log(CardWords);
+  // return (
+  //   <tbody>
+  //     {/* {" "}
+  //     {CardWords.map((word, id) => (
+  //       <Template
+  //         key={id}
+  //         tags={word.tags}
+  //         english={word.english}
+  //         transcription={word.transcription}
+  //         russian={word.russian}
+  //         name={"Edit"}
+  //       ></Template>
+  //     ))} */}
+  //   </tbody>
+  // );
 }
 
 export default TableWords;
